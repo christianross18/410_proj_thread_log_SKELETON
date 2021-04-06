03937535abe1d571624f513e8218752aaeff9486
christian <christian.ross.18@cnu.edu>
2021-04-06 15:15:53 -0400

    Work out running problem

--- src/410_proj_thread_log.cpp
+++ src/410_proj_thread_log.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : 410_proj_thread_log.cpp
-// Author      : 
-// Version     :
+// Author      : Christian Ross
+// Version     : 1.0
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
@@ -13,30 +13,42 @@
 
 using namespace std;
 
-//TODO linker errors?  Did you include the pthread library?   And set the proper dialect?
-//TODO declare globals
-
+// declare globals
+Logger l(LOG_CONSOLE);
+Logger l2(LOG_FILE);
+bool stop=false;
+vector<thread> threads;
 /***
  * 
- * TODO log info to both file and console (you can do this with 2 Logger objects)
- * TODO this function should run until main (or another thread) asks it to quit
+ *  log info to both file and console (you can do this with 2 Logger objects)
+ *  this function should run until main (or another thread) asks it to quit
  * @param info  to log
  * returns void
  */
 void fun(string info){
-
+	while(!stop){
+		l.Log(info);
+		l2.Log(info);
+	}
 }
 int main() {
 	
-	//TODO start as many threads as you have cores (see std::thread::hardware_concurrency())
-	//TODO save these threads in a vector
+	//start as many threads as you have cores (see std::thread::hardware_concurrency())
+	//save these threads in a vector
+	int t = std::thread::hardware_concurrency();
+	for(int i = 0;i<t;i++){
+		threads.push_back(thread(fun, to_string(i)));
+	}
 
-	//TODO let threads run a bit (5 seconds)
+
+	// let threads run a bit (5 seconds)
 	this_thread::sleep_for(chrono::milliseconds(5000));
 	
-	//TODO ask them all to stop
-	
-	//TODO wait for all threads to finish
-	
+	// ask them all to stop
+	stop=true;
+
+	for(int i = 0;i<t;i++){
+		threads[i].join();
+	}
 	return 0;
 }
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -2,8 +2,46 @@
  * Logger.cpp
  *
  *  Created on: Mar 29, 2021
- *      Author: keith
+ *      Author: keith & Christian
  */
 #include <iostream>
 #include "../includes/Logger.h"
 using namespace std;
+
+/***
+ * creates a logger object, if lt=FILE then log to the file given by fn
+ * if lt=LOG_CONSOLE the send log info to console via cout
+ * @param lt either LOG_FILE or LOG_CONSOLE
+ * @param fn if above is LOG_FILE then this is where info logged to
+ *           otherwise it is not used
+ */
+Logger::Logger(LOG_TYPE lt, std::string fn){
+	lock_guard<mutex> lock(mtx);
+	this->lt=lt;
+	this->fn = fn;
+	if(lt == LOG_FILE){
+		fs.open(fn);
+	}
+}
+/***
+ * Information to log, either to a file or to console
+ * @param info to log
+ */
+void Logger::Log(std::string info){
+	lock_guard<mutex> lock(mtx);
+	if(lt == LOG_FILE){
+		fs<<info;
+	}
+	else{
+		cout<<info;
+	}
+}
+
+/***
+ * close any open streams
+ */
+Logger::~Logger(){
+	if(lt == LOG_FILE){
+		fs.close();
+	}
+}
